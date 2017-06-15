6e1cbe4242b8272c5c18d99d1da8a1548f4f2b7c
Michael Zapf <m.zapf@mztx.de>
2017-06-15 21:21:15 +0200

    changed autostart to new symfony based player; update player application on boot via systemd

--- live/config/includes.chroot/usr/local/bin/run-browser.sh
+++ live/config/includes.chroot/usr/local/bin/run-browser.sh
@@ -5,7 +5,7 @@
 #
 
 
-URL=http://localhost:8001/frame.php
+URL=http://localhost/
 
 
 sed -i 's/"exited_cleanly": false/"exited_cleanly": true/' ~/.config/chromium/Default/Preferences
--- live/config/includes.chroot/usr/local/bin/shinage-update.sh
+++ live/config/includes.chroot/usr/local/bin/shinage-update.sh
@@ -1,6 +1,21 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
+
+function getShinageBranch {
+    CMDLINE=`cat /proc/cmdline`
+    for ARG in $CMDLINE
+    do
+        case $ARG in
+            SHINAGE_BRANCH=*)
+            echo -n "${ARG//SHINAGE_BRANCH=}"
+            return
+            ;;
+        esac
+    done
+    echo -n "master"
+}
+BRANCH=`getShinageBranch`
 
 # install composer globally
 cd /usr/local/bin
@@ -9,9 +24,10 @@ wget -O composer https://getcomposer.org/composer.phar
 chmod 755 composer
 
 # git clone
-mkdir -p /var/www
-cd /var/www
-git clone --recursive https://github.com/michz/shinage-player.git shinage-player
+cd /var/www/shinage-player
+git fetch --all
+git reset --hard origin/$BRANCH
+
 
 # install dependencies
 cd /var/www/shinage-player/www/signage
@@ -24,4 +40,7 @@ rm -rf /etc/nginx/sites-enabled/*
 mkdir -p /etc/nginx/conf.d/
 cp -f /var/www/shinage-player/nginx/diagnostic-htpasswd /etc/nginx/conf.d/diagnostic-htpasswd
 cp -f /var/www/shinage-player/nginx/site-signage /etc/nginx/sites-enabled/site-signage
+
+
+#TODO? restart nginx ?
 
